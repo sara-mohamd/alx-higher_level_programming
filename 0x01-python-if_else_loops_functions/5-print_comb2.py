f18cc2471f084a625a148b844112434934dca3f5
sara-mohamd <saraamohamed0038@gmail.com>
2023-10-07 11:44:33 +0300

    test 1

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-for number in range(0, 100):
-    if number == 99:
-        print("{}".format(number))
-    else:
-        print("{:02}".format(number), end=", ")
+for i in range(0, 100):
+    print("{:02}".format(i), end=(""))
+    if i != 99:
+        print(", ", end=(""))
