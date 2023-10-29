92d45bd895f4b81690efde4e663383dcf986bd40
sara-mohamd <saraamohamed0038@gmail.com>
2023-10-29 13:56:07 +0200

    test

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,4 +1,6 @@
-from add_0 import add
-a = 1
-b = 2
-print(f"{a} + {b} = {add(a, b)}")
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    print(f"{a} + {b} = {add(a, b)}")
