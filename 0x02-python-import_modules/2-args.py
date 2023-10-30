e8243383041a84bae4d79d321b1458d23dce75ac
sara-mohamd <saraamohamed0038@gmail.com>
2023-10-30 10:25:49 +0200

    task

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,6 +4,9 @@ if __name__ == "__main__":
     count = len(argv) - 1
     if count == 0:
         print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+        print(f"{1}: {argv[1]}")
     elif count > 0:
         print(f"{count} arguments:")
         for i in range(1, count + 1):
