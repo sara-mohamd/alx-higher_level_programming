36efcdaee3f802169f0ae73e8a783ee4c41a1541
sara-mohamd <saraamohamed0038@gmail.com>
2023-10-30 10:20:22 +0200

    task

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    count = len(argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count > 0:
+        print(f"{count} arguments:")
+        for i in range(1, count + 1):
+            print(f"{i}: {argv[i]}")
