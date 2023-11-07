c1d4bb40104173820c2b4a240734cf443682dace
sara-mohamd <saraamohamed0038@gmail.com>
2023-11-07 21:07:56 +0200

    test

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
 
-# if __name__ == "__main__":
-def print_list_integer(my_list=[]):
-
-    for i in range(0, 4):
-
-        print("{:d}".format(my_list[i]))
+if __name__ == "__main__":
+    def print_list_integer(my_list=[]):
+        for i in range(0, 4):
+            print("{:d}".format(my_list[i]))
