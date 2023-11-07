2b42bf3aa273557403c531a677f2eee9fcc1672f
sara-mohamd <saraamohamed0038@gmail.com>
2023-11-07 21:30:00 +0200

    test1

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def element_at(my_list, idx):
+    if my_list[idx + 1] < 0:
+        return
+    elif my_list[idx + 1] > len(my_list):
+        return
+    else:
+        return my_list[idx + 1]
+
+        
