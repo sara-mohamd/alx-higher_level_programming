c73e528d48b9761644789edfcec526b852bf3927
sara-mohamd <saramohamed0038@gmail.com>
2023-12-01 23:49:29 +0200

    done^^

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    for i in map(lambda p: f"{p[0]} : {p[1]}", sorted(a_dictionary.items())):
-        print(i)
+    # for i in map(lambda p: f"{p[0]} : {p[1]}", sorted(a_dictionary.items())):
+    #     print(i)
+    for keys, value in sorted(a_dictionary.items()):
+        print(f'{keys}: {value}')
