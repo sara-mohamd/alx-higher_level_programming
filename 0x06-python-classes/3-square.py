1de28917440ede053ffd2f588a57d5d0dfd4cd57
sara-mohamd <saramohamed0038@gmail.com>
2023-12-29 13:42:30 +0200

    done

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""New Module"""
+
+
+class Square:
+    """Task"""
+
+    def __init__(self, size=0):
+
+        """
+        Private instance attribute
+        write size with rules
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """returns the current square area"""
+        return self.__size ** 2
