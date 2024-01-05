f1168b385e48ba501b6f99a2bc0ca1fe2ecbcde5
sara-mohamd <saramohamed0038@gmail.com>
2024-01-05 06:33:36 +0200

    TDD

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,7 +10,6 @@ def add_integer(a, b=98):
     """
     Test type  of args
     otherwise raise TypeError
-    
     """
     if not isinstance(a, (float, int)):
         raise TypeError("a must be an integer")
