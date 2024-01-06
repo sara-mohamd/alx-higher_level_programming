d00ce0151414ef0842a028d8c744f0cee746ea12
sara-mohamd <saramohamed0038@gmail.com>
2024-01-06 10:45:50 +0200

    ^^

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""Q3"""
 
 
 def say_my_name(first_name, last_name=""):
-    
+    """
+    Concatinate first and last name
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print(f"My name is {first_name} {last_name}")
--- 0x07-python-test_driven_development/p.py
+++ 0x07-python-test_driven_development/p.py
@@ -5,11 +5,11 @@
 # print(add_integer('ssd'))
 
 
-matrix_divided = __import__('2-matrix_divided').matrix_divided
+# matrix_divided = __import__('2-matrix_divided').matrix_divided
 
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6]
-]
-print(matrix_divided())
+# matrix = [
+#     [1, 2, 3],
+#     [4, 5, 6]
+# ]
+# print(matrix_divided())
 
