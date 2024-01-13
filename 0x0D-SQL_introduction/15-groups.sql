b340815c0ec63ec3d8b478deb1d9e7c9042458ab
sara-mohamd <saramohamed0038@gmail.com>
2024-01-13 14:30:44 +0200

    GROUP BY

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,4 +1,4 @@
 -- script that lists the number of records with the same score in the table second_table of the database hbtn_0c_0
-SELECT score, COUNT(*) AS number
+SELECT score, COUNT(score) AS number
 FROM second_table
 GROUP BY score
