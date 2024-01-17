761487171af92a2e7ff941fcf79d7d2ac98470fa
sara-mohamd <saramohamed0038@gmail.com>
2024-01-17 17:14:29 +0200

    create database and users

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,9 @@
 -- script that creates the database hbtn_0d_2 and the user user_0d_2
 
 CREATE DATABASE hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
+IDENTIFIED BY 'user_0d_2_pwd';
+
 
 GRANT SELECT
 ON hbtn_0d_2*
