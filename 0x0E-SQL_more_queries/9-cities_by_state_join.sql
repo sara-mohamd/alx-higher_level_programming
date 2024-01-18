04bc4cd48eb621335043a7762f9157c1b3142c30
sara-mohamd <saramohamed0038@gmail.com>
2024-01-18 22:41:29 +0200

    List using inner join statment

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -2,5 +2,5 @@
 USE hbtn_0d_usa;
 
 SELECT cities.id, cities.name, states.name FROM cities
-INNER JOIN states ON states.id = cities.state_id
+JOIN states ON states.id = cities.state_id
 ORDER BY cities.id DESC;
