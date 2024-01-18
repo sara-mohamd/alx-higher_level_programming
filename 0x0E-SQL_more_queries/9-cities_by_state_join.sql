49ce03809c965f0c592e87d340a7f00c5cf81c9f
sara-mohamd <saramohamed0038@gmail.com>
2024-01-18 22:40:47 +0200

    List using inner join statment

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,6 @@
 -- script in which Each record should display: cities.id - cities.name - states.name
+USE hbtn_0d_usa;
+
 SELECT cities.id, cities.name, states.name FROM cities
 INNER JOIN states ON states.id = cities.state_id
-ORDER BY cities.id DESC
+ORDER BY cities.id DESC;
