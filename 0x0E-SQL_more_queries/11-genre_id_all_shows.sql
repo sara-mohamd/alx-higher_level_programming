c3a5f28b7d8fc90e0e30bbbbde50b315a86d6093
sara-mohamd <saramohamed0038@gmail.com>
2024-01-19 13:31:11 +0200

    Use left join

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,3 +1,5 @@
 -- Import the database dump of hbtn_0d_tvshows
 
-SELECT tv_shows.title , tv_show_genres.genre_id 
+SELECT tv_shows.title , tv_show_genres.genre_id FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_show_genres.show_id=tv_shows.id
+ORDER BY tv_shows.title , tv_show_genres.genre_id;
