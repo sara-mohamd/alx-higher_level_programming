c91fb8aef28b96a70a4fbfb098a703d0feb43568
sara-mohamd <saramohamed0038@gmail.com>
2024-01-19 13:01:16 +0200

    done

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,3 @@
+-- Import the database dump of hbtn_0d_tvshows
+
+SELECT tv_shows.title , tv_show_genres.genre_id 
--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,5 @@
 -- script that lists all the cities of California that can be found in the database hbtn_0d_usa
-SELECT id, name FROM states
-ORDER BY id DESC
+SELECT s.id, s.name FROM states s
+INNER JOIN cities c ON s.id = c.state_id
+WHERE c.name='California'
+ORDER BY id DESC;
