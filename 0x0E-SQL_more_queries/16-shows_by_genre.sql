c44f27263f6de6da194507c3e12b4871ed93658a
sara-mohamd <saramohamed0038@gmail.com>
2024-01-19 14:39:32 +0200

    Done

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -3,4 +3,4 @@
 SELECT tv_shows.title, tv_genres.name FROM  tv_genres
 RIGHT JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
 RIGHT JOIN tv_shows ON tv_show_genres.show_id=tv_shows.id
-ORDER BY tv_shows.title;
+ORDER BY tv_shows.title DESC;
