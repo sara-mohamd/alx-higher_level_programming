f17da72b4f2ee88e7cd928308dc14c54cfe0a1a7
sara-mohamd <saraamohamed0038@gmail.com>
2024-01-31 22:04:54 +0200

    using eval and return rectangle description

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python3
+""" 2ed Task """
+
+
+class Rectangle:
+    """
+        Create Class methods
+    """
+
+    def __init__(self, width=0, height=0):
+        """
+            Initialie width and height
+        """
+
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """
+        @getter
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+            Setter
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+        @getter
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+            Setter
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """
+            Calc Area
+        """
+        return self.width * self.height
+
+    def perimeter(self):
+        """
+            Calc perimeter
+        """
+        if self.height == 0 or self.width == 0:
+            return 0
+        return (self.width + self.height) * 2
+
+    def __str__(self):
+        """
+            Print the rectangle
+        """
+        result = ""
+        for _ in range(self.height):
+            result += ('#' * self.width) + '\n'
+        return result.rstrip('\n')
+
+    def __repr__(self) -> str:
+        """
+            return a string representation
+        """
+        return f"Rectangle({self.width}, {self.height})"
+
+    def eval():
+        return Rectangle.__str__
