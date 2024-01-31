d46e3740b723100cc0e83e5ee058719d16361fa0
sara-mohamd <saraamohamed0038@gmail.com>
2024-01-31 16:15:10 +0200

    rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,22 +8,21 @@ class Rectangle:
     """
 
     def __init__(self, width=0, height=0):
-
         """
         private instance attributes
         """
+
         self.__width = width
         self.__height = height
 
     @property
     def width(self):
-
         """width getter"""
+
         return self.__width
 
     @width.setter
     def width(self, value):
-
         """width setter"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
@@ -34,13 +33,11 @@ class Rectangle:
 
     @property
     def height(self):
-
         """Getter"""
         return self.__height
 
     @height.setter
     def height(self, value):
-
         """Setter"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
