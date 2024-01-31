9ffd8b74540694b8759964b91e7f23c739e00f7f
sara-mohamd <saraamohamed0038@gmail.com>
2024-01-31 16:06:58 +0200

    rectangle

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""2ed Task"""
+
+
+class Rectangle:
+    """
+    Create Class methods
+    """
+    def __init__(self, width=0, height=0):
+        """
+        private instance attributes
+        """
+        self.__width = width
+        self.__height = height
+    
+    @property
+    def width(self):
+        """width getter"""
+        return self.width
+    
+    @width.setter
+    def width(self, value):
+        """width setter"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif not (value > 0):
+            raise TypeError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """Getter"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif not (value > 0):
+            raise TypeError("height must be >= 0")
+        else:
+            self.__height = value
+
