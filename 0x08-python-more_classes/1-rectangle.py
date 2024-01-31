bf64c37e6571654a19948ee525bc38ba0ae2810d
sara-mohamd <saraamohamed0038@gmail.com>
2024-01-31 16:38:56 +0200

    try

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,48 +1,52 @@
 #!/usr/bin/python3
-"""2ed Task"""
+""" 2nd task """
 
 
 class Rectangle:
     """
-    Create Class methods
+    class with methods and instance attributes
     """
 
     def __init__(self, width=0, height=0):
         """
-        private instance attributes
+        Initialie width and height
         """
 
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
-        """width getter"""
-
+        """
+        @getter
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
-        """width setter"""
-
-        if not isinstance(value, int):
+        """
+           Setter
+        """
+        if type(value) is not int:
             raise TypeError("width must be an integer")
-        elif not (value > 0):
-            raise TypeError("width must be >= 0")
-
+        elif value < 0:
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
     def height(self):
-        """Getter"""
+        """
+        @getter
+        """
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Setter"""
-
-        if not isinstance(value, int):
+        """
+           Setter
+        """
+        if type(value) is not int:
             raise TypeError("height must be an integer")
-        elif not (value > 0):
-            raise TypeError("height must be >= 0")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
         self.__height = value
