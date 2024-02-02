f03edbb5c95a367da38553d7e6a38dab9a3e8e2f
sara-mohamd <saraamohamed0038@gmail.com>
2024-02-02 15:58:59 +0200

    done

--- 0x09-python-everything_is_object/test.py
+++ 0x09-python-everything_is_object/test.py
@@ -1,5 +1,13 @@
-a = [1, 2, 3]
-print(id (a))
+n1 = 100
+n2 = 100
+print(n1 is n2)
+print('-' * 5)
 
-a += [4]
-print(id (a))
+x = ()
+y = ()
+print(x is y)
+print('-' * 5)
+t1 = (1, 2)
+t2 = (1, 2)
+
+print(t1 is t2)
