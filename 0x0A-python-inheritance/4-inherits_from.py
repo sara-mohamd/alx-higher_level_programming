ad411b1680a7b9e7a62325aeaa933b872b3acf45
sara-mohamd <saraamohamed0038@gmail.com>
2024-02-05 14:03:53 +0200

    test

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-""" Q3 """
+""" Q4 """
 
 
-def is_kind_of_class(obj, a_class):
+def inherits_from(obj, a_class):
     """
     returns True if the object is an instance of the specified class
     otherwise False
     """
 
-    return isinstance(obj, a_class)
+    return issubclass(type(obj), a_class) and type(obj) != a_class
