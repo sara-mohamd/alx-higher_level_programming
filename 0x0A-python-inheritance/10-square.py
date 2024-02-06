cc5e5f26b55eee4347e3149b0184ee63c79ec865
sara-mohamd <saraamohamed0038@gmail.com>
2024-02-06 11:57:01 +0200

    Square class

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Q10 """
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Another Shape class """
+
+    def __init__(self, size):
+        """ Initializing method """
+        super().__init__(size, size)
+
+        super().integer_validator('size', size)
+        self.__size = size
+
+    def area(self):
+        """ Calc Area """
+        return (self.__size**2)
