aa2214175192ed9c674a6d5b27b91a547495896b
sara-mohamd <saraamohamed0038@gmail.com>
2024-02-11 09:58:15 +0200

    Serlializing a python object

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Task 5 """
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    writes an Object to a text file, using a JSON representation
+    and use with statment
+    """
+    with open(filename, 'w') as data:
+
+        data.write(json.dumps(my_obj))
