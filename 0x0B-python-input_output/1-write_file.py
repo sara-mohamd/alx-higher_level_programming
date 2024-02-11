41a557acb6f61d579acfb2b33814ac2d71980954
sara-mohamd <saraamohamed0038@gmail.com>
2024-02-11 07:29:42 +0200

    write into a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,5 +3,9 @@
 
 
 def write_file(filename="", text=""):
+    """
+    function writes a string to text file
+    return number of chars written
+    """
     with open(filename, "w", encoding="utf-8") as file:
         return file.write(text)
