8337609b87568ed4bbb167a7fc363ef8d93d826b
sara-mohamd <saraamohamed0038@gmail.com>
2024-02-15 17:00:44 +0200

    Wonderfull question

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Task 9 """
+
+
+class Student:
+    """ Class define a student """
+
+    def __init__(self, first_name, last_name, age):
+        """ Initializing method """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if (type(attrs) == list
+                and all(type(var) == str for var in attrs)):
+            return {key: getattr(self, key) for key in
+                    attrs if hasattr(self, key)}
+
+        return self.__dict__
--- 0x0B-python-input_output/p.py
+++ 0x0B-python-input_output/p.py
@@ -1,19 +1,34 @@
 #!/usr/bin/python3
 import json
 
-json_dict = """{
-    "id": 12,
-    "name": "John",
-    "places": ["San Francisco", "Tokyo"],
-    "is_active": true,
-    "info": {
-        "age": 36,
-        "average": 3.14
-    }
-}"""
+# json_dict = """{
+#     "id": 12,
+#     "name": "John",
+#     "places": ["San Francisco", "Tokyo"],
+#     "is_active": true,
+#     "info": {
+#         "age": 36,
+#         "average": 3.14
+#     }
+# }"""
 
-print(type(json_dict))
+# print(type(json_dict))
 
 
-data = json.loads(json_dict)
-print(data)
+# data = json.loads(json_dict)
+# print(data)
+
+#   ------------------------
+#   Learn getattr, hasatter
+#   ------------------------
+
+
+class Person:
+    x = 30
+
+
+obj = Person()
+Person.x = 15
+if hasattr(obj, 'x'):
+    print(getattr(obj, 'x'))
+
