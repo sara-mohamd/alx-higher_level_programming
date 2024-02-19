7837368f1a3728f405d23c09c4525b58fbc7b987
sara-mohamd <saraamohamed0038@gmail.com>
2024-02-20 00:05:32 +0200

    respect that task

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
 """ Task 10 """
-from models.base import Base
+from models.rectangle import Rectangle
 
 
-class Square(Base):
-    ...
+class Square(Rectangle):
+    """
+    it's already Almost a Circle !
+    """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """ Constructor """
+        super().__init__(width=size, height=size, x=x, y=y, id=id)
+
+    def __str__(self):
+        """ str representation """
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
