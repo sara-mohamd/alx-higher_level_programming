30bbeec303103242879af5c1a8c17978314c2348
sara-mohamd <saraamohamed0038@gmail.com>
2024-02-20 17:46:53 +0200

    check

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -30,11 +30,11 @@ class Base:
         """
         new_list = []
         file_name = cls.__name__ + ".json"
-        if list_objs is None:
-            with open(file_name, 'w') as file:
-                file.write(json.dump(list_objs))
+        # if list_objs is None:
         #     with open(file_name, 'w') as file:
-        #     return
+        #         file.write(json.dump(list_objs))
+        # #     with open(file_name, 'w') as file:
+        # #     return
         for i in list_objs:
             new_list.append(i.to_dictionary())
         with open(file_name, 'w') as file:
