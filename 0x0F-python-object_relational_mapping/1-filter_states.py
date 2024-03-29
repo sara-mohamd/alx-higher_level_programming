8a6f8cf3e1ae28817292a09ea0f170baa97ef2bf
sara-mohamd <saraamohamed0038@gmail.com>
2024-03-29 09:47:45 +0200

    test second ques

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -16,7 +16,8 @@ if __name__ == "__main__":
             db=argv[3]
     ) as connection:
         with connection.cursor() as cur:
-            cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC;")
+            cur.execute("SELECT * FROM states "
+                        "WHERE name LIKE 'N%' ORDER BY id ASC;")
         result = cur.fetchall()
         for data in result:
             print(data)
