2bfc6bf4463495a096d1ef125a114eb425f42e25
sara-mohamd <saraamohamed0038@gmail.com>
2024-04-28 14:12:02 +0300

    nested loop

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-///
 if (isNaN(process.argv[2])) {
   console.log('Missing size');
-} else {
+} else if (process.argv[2] > 0){
   for (let i = 0; i < process.argv[2]; i++) {
     let temp = '';
     for (let j = 0; j <= process.argv[2]; j++) { temp += 'x'; }
     console.log(temp);
   }
-}
+} 
