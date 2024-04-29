15f61dfc95c0001a4b9d75c2bb31ce9d1d70f621
sara-mohamd <saraamohamed0038@gmail.com>
2024-04-29 22:52:43 +0300

    occurance

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+exports.nbOccurences = function (list, searchElement) {
+  let counter = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) { counter++; }
+  }
+  return counter;
+};
