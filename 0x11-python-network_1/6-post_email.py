340c38a230178a2370a5f9b0ba5da6a914f71ad0
sara-mohamd <saraamohamed0038@gmail.com>
2024-06-09 14:36:26 +0300

    requests.post

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Python script that takes in a URL and an email address, sends a POST request """
+""" takes in a URL and an email address, sends a POST request """
 if __name__ == "__main__":
     from requests import post
     from sys import argv
