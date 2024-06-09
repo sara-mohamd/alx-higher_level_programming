2645065ffb0682477879c1cda139d8430723d60e
sara-mohamd <saraamohamed0038@gmail.com>
2024-06-09 14:35:06 +0300

    requests.post

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Python script that takes in a URL and an email address, sends a POST request """
+if __name__ == "__main__":
+    from requests import post
+    from sys import argv
+
+    file = post(argv[1], data={'email': argv[2]})
+    print(file.text)
