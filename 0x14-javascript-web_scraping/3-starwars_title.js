3f1c01a794882adc4628d520344ca6bf783beba9
sara-mohamd <saraamohamed0038@gmail.com>
2024-09-13 21:29:25 +0300

    check this time

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,27 +1,12 @@
 #!/usr/bin/node
-// const request = require('request');
-// request(`https://swapi-api.alx-tools.com/api/films/:${process.argv[2]}`, (err, res, body) => {
-//   if (err) {
-//     console.log(err);
-//   } else if (res.statusCode === 200) {
-//     // const json = ;
-//     console.log((JSON.parse(body)).title);
-//   } else {
-//     console.log(`Error: ${res.statusCode}`);
-//   }
-// });
-
 const request = require('request');
-const episodeNum = process.argv[2];
-const API_URL = 'https://swapi-api.hbtn.io/api/films/';
-
-request(API_URL + episodeNum, (err, res, body) => {
+request(`https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`, (err, res, body) => {
   if (err) {
     console.log(err);
   } else if (res.statusCode === 200) {
-    const resJSON = JSON.parse(body);
-    console.log(resJSON.title);
+    // const json = ;
+    console.log((JSON.parse(body)).title);
   } else {
-    console.log('Error code: ' + res.statusCode);
+    console.log(`Error: ${res.statusCode}`);
   }
 });
