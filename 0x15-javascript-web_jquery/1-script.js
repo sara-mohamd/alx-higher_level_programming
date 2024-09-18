e8370a120039218e09bced42ec773e0aedc39e4b
sara-mohamd <saraamohamed0038@gmail.com>
2024-09-18 18:41:37 +0300

    adding JSDoc

--- 0x15-javascript-web_jquery/1-script.js
+++ 0x15-javascript-web_jquery/1-script.js
@@ -1,1 +1,5 @@
+/**
+ * - updates the text color of the <header> element to red (#FF0000)
+ * - Using jQuery
+ */
 $('header').css('color', '#ff0000');
--- 0x15-javascript-web_jquery/2-script.js
+++ 0x15-javascript-web_jquery/2-script.js
@@ -1,3 +1,8 @@
+/**
+ * - When the user clicks on the tag DIV#red_header
+ * when the user clicks on the tag DIV#red_header.
+ */
+
 $('DIV#red_header').click(() => {
   $('header').css('color', '#ff0000');
 });
