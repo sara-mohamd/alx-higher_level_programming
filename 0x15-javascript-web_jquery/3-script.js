3729998714e1662f5c40e9d7646ef23eb25407ef
sara-mohamd <saraamohamed0038@gmail.com>
2024-09-18 18:43:56 +0300

    adding JSDoc

--- 0x15-javascript-web_jquery/3-script.js
+++ 0x15-javascript-web_jquery/3-script.js
@@ -1,3 +1,7 @@
+/**
+ * - Add the class red to the <header> element
+ * - When the user clicks on the tag DIV#red_header
+ */
 $('DIV#red_header').click(function () {
   $('header').addClass('red');
 });
